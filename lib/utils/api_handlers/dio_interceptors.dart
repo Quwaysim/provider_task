d6ff98560d747b719e54f2fb5e74a7e46e08b0e0
Quwaysim <qaasim405@gmail.com>
2022-02-16 14:44:08 +0100

    setup dio interceptors

---
+++ lib/utils/api_handlers/dio_interceptors.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
+
+class DioInterceptor implements Interceptor {
+  final log = Logger();
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    log.i('ENDPOINT: ' + err.requestOptions.uri.toString());
+    log.i('STATUSCODE: ' + err.error.toString());
+    log.i('MESSAGE: ' + (err.response?.data ?? err.message).toString());
+    handler.next(err);
+  }
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    log.i('METHOD: ' + options.method);
+    log.i('ENDPOINT: ' + options.uri.toString());
+    //TODO: delete this later
+    log.i('HEADERS: ' + options.headers.toString());
+    log.i('DATA: ' + (options.data ?? options.queryParameters).toString());
+    handler.next(options);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    log.i('ENDPOINT: ' + response.requestOptions.uri.toString());
+    log.i('STATUSCODE: ' + response.statusCode.toString());
+    log.i('DATA: ' + response.data.toString());
+    handler.next(response);
+  }
+}

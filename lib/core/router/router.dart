fd5917c7396b8cae0afa82b7044d244af484225f
Quwaysim <qaasim405@gmail.com>
2022-02-18 16:28:34 +0100

    updated routes

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider_task/core/router/route_paths.dart';
-import 'package:provider_task/ui/registration/registration_page.dart';
-import 'package:provider_task/ui/verification/verification_page.dart';
+import 'package:provider_task/ui/views/registration_page.dart';
+import 'package:provider_task/ui/views/update_profile.dart';
+import 'package:provider_task/ui/views/user_profile.dart';
+import 'package:provider_task/ui/views/verification_page.dart';
 
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -10,10 +12,10 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => RegistrationPage());
       case RoutePaths.verification:
         return MaterialPageRoute(builder: (_) => VerificationPage());
-      // case RoutePaths.userProfile:
-      //   return MaterialPageRoute(builder: (_) => ());
-      // case RoutePaths.updateProfile:
-      //   return MaterialPageRoute(builder: (_) => ());
+      case RoutePaths.userProfile:
+        return MaterialPageRoute(builder: (_) => const UserProfilePage());
+      case RoutePaths.updateProfile:
+        return MaterialPageRoute(builder: (_) => const UpdateProfilePage());
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
